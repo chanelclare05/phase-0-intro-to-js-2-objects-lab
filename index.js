2a99e543c84e18e2752187e9c70c80fda9f2a940
Chanel Donahoe <cdonahoe05@gmail.com>
2021-07-15 14:46:52 +1000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+let employee = {
+    name: 'Chanel',
+    streetAddress: '9 Academy St'
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newObj = {...obj};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+   const newObj = Object.assign({}, obj);
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
